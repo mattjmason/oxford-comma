bd692507afc8f03341adab5067eeb1db7328532e
Matt Mason <masonmj10@gmail.com>
2020-11-17 20:23:24 -0500

    commit tree 756bc103be4adca061e23a5dedffa7a44823017a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
 
+    if array.length == 1
+    array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else 
+        lastWord = array.pop
+        array.join(", ") << ", and #{lastWord}"
+    end 
+
 end
